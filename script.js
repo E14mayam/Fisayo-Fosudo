a1f8793585954c5cefe5a8546547190b4033f343
E14mayam <93450333+E14mayam@users.noreply.github.com>
2022-05-27 20:10:27 +0100

    link chages

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const hyperlink = document.querySelectorAll(".hyperlink a");
 
 hyperlink.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log("you clicked me");
   document.querySelector(this.getAttribute("href")).scrollIntoView({
     behavior: "smooth",
   });
