c7f1b7552c860218cb308fb0e4dde492a8658c4f
E14mayam <93450333+E14mayam@users.noreply.github.com>
2022-05-27 20:16:44 +0100

    link fix

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const hyperlink = document.querySelector(".hyperlink a");
 
-hyperlink.addEventListener("click", function (e) {
-  e.preventDefault();
-  document.querySelector(this.getAttribute("href")).scrollIntoView({
-    behavior: "smooth",
-  });
-});
